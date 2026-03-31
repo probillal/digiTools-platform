1c95fd3d5d29fc73d90dcf9bf4ccc1a2e8df72b1
probillal <smbillalhossain273@gmail.com>
2026-03-31 22:29:15 +0600

    added navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <div>
-        <h2></h2>
+        <Navbar></Navbar>
       </div>
     </>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100 shadow-sm px-4 lg:px-10">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-30 p-2 shadow"
+          >
+            <li>
+              <a>Products</a>
+            </li>
+            <li>
+              <a>Features</a>
+            </li>
+            <li>
+              <a>Pricing</a>
+            </li>
+            <li>
+              <a>Testimonials</a>
+            </li>
+            <li>
+              <a>FAQ</a>
+            </li>
+          </ul>
+        </div>
+        <h2 className="text-2xl">DigiTools</h2>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a>Products</a>
+          </li>
+          <li>
+            <a>Features</a>
+          </li>
+          <li>
+            <a>Pricing</a>
+          </li>
+          <li>
+            <a>Testimonials</a>
+          </li>
+          <li>
+            <a>FAQ</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end gap-4">
+        <a className="btn w-12 h-12 rounded-full">
+          <FaShoppingCart className="text-xl" />
+        </a>
+        <a className="btn rounded-2xl">Login</a>
+        <a className="btn rounded-2xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">
+          Button
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
